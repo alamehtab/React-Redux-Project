29d929ff04b4ffebd6da68c26e61fca96eec4a65
Mehtab Alam <alamehtab2002@gmail.com>
2025-07-07 09:49:42 +0530

    changes made

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Cart from './features/cart/Cart'
 import { useDispatch, useSelector } from 'react-redux'
 import { fetchItemsAsync } from './features/cart/cartSlice'
 
+
 function App() {
   const [count, setCount] = useState(0)
   const [showCart, setShowCart] = useState(false)
@@ -17,7 +18,7 @@ function App() {
 
   return (
     <>
-      <button style={{ position: 'fixed', top: '20px', right: '100px', zIndex: 1000 }} onClick={() => setShowCart(!showCart)}>{showCart ? 'Hide Cart' : 'Show Cart'} [{items.length}] </button>
+      <button style={{ position: 'fixed', top: '100px', right: '100px', zIndex: 1000,fontWeight:'bold' }} onClick={() => setShowCart(!showCart)}>{showCart ? 'Hide Cart' : 'Show Cart'} [{items.length}] </button>
       <div style={{marginTop:'60px'}}>
         {
           showCart ? <Cart /> : <Product />
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,20 @@ import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 import productsReducer from './features/products/productSlice.js'
 import cartReducer from './features/cart/cartSlice.js'
+import { BrowserRouter } from 'react-router-dom'
 
 export const store = configureStore({
-  reducer:{
-    product:productsReducer,
-    cart:cartReducer
+  reducer: {
+    product: productsReducer,
+    cart: cartReducer
   }
 })
 
 createRoot(document.getElementById('root')).render(
-    <Provider store={store}>
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-    </Provider>
+    </BrowserRouter>
+
+  </Provider>
 )
